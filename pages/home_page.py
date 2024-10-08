bb038fd060206c0f8c47de3204e8f6da6dec15e7
unknown <daniel.m.bassett@outlook.com>
2024-10-08 06:11:03 -0500

    added other grant programs to the data

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import glob
 
 import streamlit as st
 import pandas as pd
@@ -35,22 +36,25 @@ def load_data(path):
 def clear_text():
     st.session_state['abstract'] = ""
 
-def find_grants(abstract):
+def find_grants():
     if len(st.session_state.abstract) > 0:
-        st.session_state['embedding'] = insight_grant.get_embedding(st.session_state.abstract)
-    
-    if 'embedding' in st.session_state:
-        cosine_scores = np.dot(np.stack(st.session_state.grants['embeddings']), st.session_state.embedding)
-        st.session_state.grants['cosine_scores'] = cosine_scores
+        embedding = insight_grant.get_embedding(st.session_state.abstract)
+        cosine_scores = np.dot(np.stack(st.session_state.grants['embeddings']), embedding)
+        grants_with_scores = st.session_state.grants.copy()
+        grants_with_scores['cosine_scores'] = cosine_scores
+        st.session_state['results'] = grants_with_scores.sort_values(by='cosine_scores', ascending=False)
+        # st.session_state.grants['cosine_scores'] = cosine_scores
+        # st.write(st.session_state.grants)
 
 @st.fragment
 def get_abstract():
-    abstract = st.text_area('Enter Abstract', placeholder='Press Ctrl+Enter to Apply', max_chars=3000, height= 350, key='abstract')
+    st.text_area('Enter Abstract', placeholder='Press Ctrl+Enter to Apply', max_chars=3000, height= 350, key='abstract')
 
 
 ######## LOAD DATA ########
 # df = load_data('data/hhs.parquet')
-st.session_state['grants'] = load_data('data/hhs.parquet')
+# st.session_state['grants'] = load_data('data/hhs.parquet')
+st.session_state['grants'] = pd.concat([load_data(path) for path in glob.glob('data/grants/**/*.parquet', recursive=True)])
 
 
 ######## SHOW DATAFRAME ########
@@ -67,18 +71,18 @@ buttons_container = st.container()
 output_placeholder = st.container()
 
 with buttons_container:
-    find_col, del_col, _ = st.columns([4, 4, 12])
+    find_col, del_col, _ = st.columns([2, 2, 13])
 
     with find_col:
-        st.button('Find Grants', on_click=find_grants, args=(st.session_state.abstract))
+        st.button('Search', on_click=find_grants)
     
     with del_col:
-        st.button('Delete Abstract', on_click=clear_text)
+        st.button('Delete', on_click=clear_text)
 
-if 'cosine_scores' in st.session_state:
-    st.session_state['results'] = st.session_state.cosine_scores
-    st.dataframe(, hide_index=False)
+if 'results' in st.session_state:
+    st.dataframe(st.session_state.results.drop(columns=['embeddings', 'scraped_at']), hide_index=True)
     # for result in st.session_state.embedding:
     #     st.divider()
     #     st.write(embedding)
-        
+
+# st.write(st.session_state.grants.drop(columns='embeddings')['cosine_scores'])
