ce05ebf903e96c6c03748e53864881ea325a9768
unknown <daniel.m.bassett@outlook.com>
2024-10-10 09:17:26 -0500

    passworded

--- app.py
+++ app.py
@@ -9,32 +9,32 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
-# def check_password():
-#     """Returns `True` if the user had the correct password."""
+def check_password():
+    """Returns `True` if the user had the correct password."""
 
-#     def password_entered():
-#         """Checks whether a password entered by the user is correct."""
-#         if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
-#             st.session_state["password_correct"] = True
-#             del st.session_state["password"]  # Don't store the password.
-#         else:
-#             st.session_state["password_correct"] = False
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
 
-#     # Return True if the password is validated.
-#     if st.session_state.get("password_correct", False):
-#         return True
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
 
-#     # Show input for password.
-#     st.text_input(
-#         "Password", type="password", on_change=password_entered, key="password"
-#     )
-#     if "password_correct" in st.session_state:
-#         st.error("😕 Password incorrect")
-#     return False
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
 
 
-# if not check_password():
-#     st.stop()
+if not check_password():
+    st.stop()
 
 ######## APP LAYOUT ########
 home_page = st.Page(page='pages/home_page.py', title='Home Page')
